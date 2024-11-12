d97459f82d151610be98b0af97260a6dd34389c7
NikhitaSyamR <157688577+NikhitaSyamR@users.noreply.github.com>
2024-11-13 01:48:19 +0530

    Add files via upload

---
+++ Arrays1.java
@@ -0,0 +1,44 @@
+//Find Minimum and Maximum in Array
+/*import java.util.*;
+public class Arrays1 {
+    public  static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number: ");
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int max=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            if(max<arr[i]){
+                max=arr[i];
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(min>arr[i]){
+                min=arr[i];
+            }
+        }
+        System.out.println("The maximum number is "+max);
+        System.out.println("The minimum number is "+min);
+    }
+}
+
+//Reverse an Array
+import java.util.*;
+public class Arrays1 {
+    public  static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number: ");
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=n-1;i>=0;i--){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}*/
